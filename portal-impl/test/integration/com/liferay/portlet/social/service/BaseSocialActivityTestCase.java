a5e58ba2ba3509940ff0042370fc7a9cae539f18
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-05-31 08:26:01 +0200

    LPS-35373 Rollback

--- portal-impl/test/integration/com/liferay/portlet/social/service/BaseSocialActivityTestCase.java
+++ portal-impl/test/integration/com/liferay/portlet/social/service/BaseSocialActivityTestCase.java
@@ -24,7 +24,6 @@ import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.UserTestUtil;
 import com.liferay.portlet.asset.model.AssetEntry;
 import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
-import com.liferay.portlet.social.util.SocialActivityHierarchyThreadLocal;
 import com.liferay.portlet.social.util.SocialActivityTestUtil;
 import com.liferay.portlet.social.util.SocialConfigurationUtil;
 
@@ -61,16 +60,12 @@ public class BaseSocialActivityTestCase {
 		_actorUser = UserTestUtil.addUser("actor", _group.getGroupId());
 		_creatorUser = UserTestUtil.addUser("creator", _group.getGroupId());
 
-		_assetEntry = SocialActivityTestUtil.addAssetEntry(
+		_assetEntry = SocialActivityTestUtil.addAsset(
 			_creatorUser, _group, null);
-
-		SocialActivityHierarchyThreadLocal.clear();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		SocialActivityHierarchyThreadLocal.clear();
-
 		if (_actorUser != null) {
 			UserLocalServiceUtil.deleteUser(_actorUser);
 
