6cc628f87e9218d3e55c19435a2a0a8977440867
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-04 11:19:29 +0100

    LPS-44013 Set portletPreferencesLS to mock service

--- portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
@@ -1307,6 +1307,10 @@ public class PortletPreferencesLocalServiceTest {
 			(PortletLocalService)PortalBeanLocatorUtil.locate(
 				PortletLocalService.class.getName()));
 
+		mockservice.setPortletPreferencesLocalService(
+			(PortletPreferencesLocalService)PortalBeanLocatorUtil.locate(
+				PortletPreferencesLocalService.class.getName()));
+
 		return mockservice;
 	}
 
