28ee215a80505b020cce7173ab58a44dcf277c61
cgoncas <cristina.gonzalez@liferay.com>
2014-04-09 10:47:57 +0200

    Test Fixes - The way the test set the TaskContext was no correct, because the method needs a serialized Map, and a random string was setting instead.

--- portal-impl/test/integration/com/liferay/portal/service/persistence/BackgroundTaskPersistenceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/persistence/BackgroundTaskPersistenceTest.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
@@ -45,6 +46,7 @@ import org.junit.runner.RunWith;
 
 import java.io.Serializable;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -561,7 +563,13 @@ public class BackgroundTaskPersistenceTest {
 
 		backgroundTask.setTaskExecutorClassName(ServiceTestUtil.randomString());
 
-		backgroundTask.setTaskContext(ServiceTestUtil.randomString());
+		Map<String, Serializable> taskContextMap =
+			new HashMap<String, Serializable>();
+
+		taskContextMap.put(ServiceTestUtil.randomString(), Boolean.TRUE);
+
+		backgroundTask.setTaskContext(
+			JSONFactoryUtil.serialize(taskContextMap));
 
 		backgroundTask.setCompleted(ServiceTestUtil.randomBoolean());
 
