95ce026ad91b0eeee829e7ab5c3f7108c3e79a5f
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-08-28 13:47:46 +0200

    LPS-48577 Extension to launch PersistenceTest environment

---
+++ modules/test/persistence-arquillian-extension/src/com/liferay/arquillian/persistence/extension/PersistenceTestScenarioExtension.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.arquillian.persistence.extension;
+
+import com.liferay.arquillian.extension.descriptor.SpringDescriptor;
+import com.liferay.arquillian.extension.descriptor.SpringDescriptorImpl;
+import com.liferay.arquillian.persistence.extension.databaseBuilder.descriptor.SpringDescriptorBasabaseBuilderImpl;
+import com.liferay.arquillian.persistence.extension.databaseBuilder.observer.InitializeDatabaseObserver;
+
+import org.jboss.arquillian.core.spi.LoadableExtension;
+
+/**
+ * @author Cristina González Castellano
+ */
+public class PersistenceTestScenarioExtension implements LoadableExtension {
+
+	@Override
+	public void register(ExtensionBuilder builder) {
+		builder.observer(InitializeDatabaseObserver.class);
+
+		builder.override(
+			SpringDescriptor.class, SpringDescriptorImpl.class,
+			SpringDescriptorBasabaseBuilderImpl.class);
+	}
+
+}
