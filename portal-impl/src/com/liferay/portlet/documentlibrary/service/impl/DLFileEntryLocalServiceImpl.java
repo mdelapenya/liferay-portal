6eac82f009956910d316af27622ce2d1051b63ec
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-09-03 11:58:33 +0200

    Revert "LPS-49619 upgrade"
    This reverts commit 8e2fa015f4cd7595b1151a6522c6dc0663fdb8d8.

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -170,7 +170,7 @@ public class DLFileEntryLocalServiceImpl
 		String name = String.valueOf(
 			counterLocalService.increment(DLFileEntry.class.getName()));
 		String extension = DLAppUtil.getExtension(title, sourceFileName);
-		String filename = DLUtil.getFilename(title, extension);
+		String filename = getFilename(title, extension);
 
 		if (fileEntryTypeId == -1) {
 			fileEntryTypeId =
@@ -2009,6 +2009,19 @@ public class DLFileEntryLocalServiceImpl
 		}
 	}
 
+	protected String getFilename(String title, String extension) {
+		String filename = StringUtil.replace(
+			title, StringPool.SLASH, StringPool.UNDERLINE);
+
+		if (Validator.isNotNull(extension) &&
+			!filename.endsWith(StringPool.PERIOD + extension)) {
+
+			filename += StringPool.PERIOD + extension;
+		}
+
+		return filename;
+	}
+
 	protected String getNextVersion(
 			DLFileEntry dlFileEntry, boolean majorVersion, int workflowAction)
 		throws PortalException {
@@ -2346,7 +2359,7 @@ public class DLFileEntryLocalServiceImpl
 				}
 			}
 
-			String filename = DLUtil.getFilename(title, extension);
+			String filename = getFilename(title, extension);
 
 			Date now = new Date();
 
