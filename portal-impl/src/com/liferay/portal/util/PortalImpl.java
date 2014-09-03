mdelapenya/liferay-portal

commit b7f1edbdf52e0fd5b545a0541f1671a8ee2d75f4

LPS-48834  URLs for links on portlets are pointing to the virtual host instead of the url the portal is being accessed through

portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -7955,11 +7955,22 @@ public class PortalImpl implements Portal {
 
 		boolean useGroupVirtualHostName = false;
 
-		if (canonicalURL ||
-			!StringUtil.equalsIgnoreCase(
-				themeDisplay.getServerName(), _LOCALHOST)) {
-
+		if (canonicalURL) {
 			useGroupVirtualHostName = true;
+		}
+
+		for (String virtualHost : PropsValues.VIRTUAL_HOSTS_VALID_HOSTS) {
+			if (StringUtil.equalsIgnoreCase(
+					themeDisplay.getServerName(), virtualHost) ||
+				StringUtil.wildcardMatches(
+					themeDisplay.getServerName(), virtualHost,
+					CharPool.QUESTION, CharPool.STAR,
+					CharPool.PERCENT, false)) {
+
+				useGroupVirtualHostName = false;
+
+				break;
+			}
 		}
 
 		long refererPlid = themeDisplay.getRefererPlid();

commit 474c5e1c32009d548dff193ded9b61f512092248

LPS-48834 (Temporary fix) Keep original logic that was broken by b7f1edbdf52e0fd5b545a0541f1671a8ee2d75f4 and its review
As it affects ee-6.2.x and master, this fix turns back original logic for getGroupFriendlyURL method, because the rollback is pretty difficult as too many commits were introduced after offending ones

portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -5931,8 +5931,14 @@ public class PortalImpl implements Portal {
 
 	@Override
 	public String getValidPortalDomain(long companyId, String domain) {
-		if (isValidPortalDomain(companyId, domain)) {
-			return domain;
+		for (String virtualHost : PropsValues.VIRTUAL_HOSTS_VALID_HOSTS) {
+			if (StringUtil.equalsIgnoreCase(domain, virtualHost) ||
+				StringUtil.wildcardMatches(
+					domain, virtualHost, CharPool.QUESTION, CharPool.STAR,
+					CharPool.PERCENT, false)) {
+
+				return domain;
+			}
 		}
 
 		if (_log.isWarnEnabled()) {
@@ -7978,9 +7984,30 @@ public class PortalImpl implements Portal {
 
 		String portalURL = themeDisplay.getPortalURL();
 
-		if (isUseGroupVirtualHostName(
-				group, privateLayoutSet, themeDisplay, canonicalURL)) {
+		boolean useGroupVirtualHostName = false;
 
+		if (canonicalURL ||
+			!StringUtil.equalsIgnoreCase(
+				themeDisplay.getServerName(), _LOCALHOST)) {
+
+			useGroupVirtualHostName = true;
+		}
+
+		long refererPlid = themeDisplay.getRefererPlid();
+
+		if (refererPlid > 0) {
+			Layout refererLayout = LayoutLocalServiceUtil.fetchLayout(
+				refererPlid);
+
+			if ((refererLayout != null) &&
+				((refererLayout.getGroupId() != group.getGroupId()) ||
+					(refererLayout.isPrivateLayout() != privateLayoutSet))) {
+
+				useGroupVirtualHostName = false;
+			}
+		}
+
+		if (useGroupVirtualHostName) {
 			String virtualHostname = getVirtualHostname(layoutSet);
 
 			String portalDomain = HttpUtil.getDomain(portalURL);