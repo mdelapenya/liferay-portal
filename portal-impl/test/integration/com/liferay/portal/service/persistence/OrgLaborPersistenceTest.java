cce1b4903e53b4a296dc4580c0bda16642dabb2a
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-09-05 12:38:12 +0200

    LPS-48577 Autogenerated

--- portal-impl/test/integration/com/liferay/portal/service/persistence/OrgLaborPersistenceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/persistence/OrgLaborPersistenceTest.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.service.persistence;
 
+import com.liferay.arquillian.persistence.extension.databaseBuilder.annotation.DatabaseBuilder;
+
 import com.liferay.portal.NoSuchOrgLaborException;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
@@ -23,23 +25,17 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.template.TemplateException;
-import com.liferay.portal.kernel.template.TemplateManagerUtil;
-import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.util.IntegerWrapper;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import com.liferay.portal.model.OrgLabor;
 import com.liferay.portal.service.OrgLaborLocalServiceUtil;
-import com.liferay.portal.test.TransactionalTestRule;
-import com.liferay.portal.test.runners.PersistenceIntegrationJUnitTestRunner;
-import com.liferay.portal.tools.DBUpgrader;
 import com.liferay.portal.util.test.RandomTestUtil;
+
+import org.jboss.arquillian.junit.Arquillian;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
@@ -54,25 +50,11 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * @generated
- */
-@RunWith(PersistenceIntegrationJUnitTestRunner.class)
+* @generated
+*/
+@DatabaseBuilder
+@RunWith(Arquillian.class)
 public class OrgLaborPersistenceTest {
-	@ClassRule
-	public static TransactionalTestRule transactionalTestRule = new TransactionalTestRule(Propagation.REQUIRED);
-
-	@BeforeClass
-	public static void setupClass() throws TemplateException {
-		try {
-			DBUpgrader.upgrade();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		TemplateManagerUtil.init();
-	}
-
 	@After
 	public void tearDown() throws Exception {
 		Iterator<OrgLabor> iterator = _orgLabors.iterator();
--- portal-impl/test/integration/com/liferay/portlet/expando/service/persistence/ExpandoTablePersistenceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/expando/service/persistence/ExpandoTablePersistenceTest.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portlet.expando.service.persistence;
 
+import com.liferay.arquillian.persistence.extension.databaseBuilder.annotation.DatabaseBuilder;
+
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
@@ -22,17 +24,11 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.template.TemplateException;
-import com.liferay.portal.kernel.template.TemplateManagerUtil;
-import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.util.IntegerWrapper;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.test.TransactionalTestRule;
-import com.liferay.portal.test.runners.PersistenceIntegrationJUnitTestRunner;
-import com.liferay.portal.tools.DBUpgrader;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.test.RandomTestUtil;
 
@@ -41,10 +37,10 @@ import com.liferay.portlet.expando.model.ExpandoTable;
 import com.liferay.portlet.expando.model.impl.ExpandoTableModelImpl;
 import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
 
+import org.jboss.arquillian.junit.Arquillian;
+
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
@@ -59,25 +55,11 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * @generated
- */
-@RunWith(PersistenceIntegrationJUnitTestRunner.class)
+* @generated
+*/
+@DatabaseBuilder
+@RunWith(Arquillian.class)
 public class ExpandoTablePersistenceTest {
-	@ClassRule
-	public static TransactionalTestRule transactionalTestRule = new TransactionalTestRule(Propagation.REQUIRED);
-
-	@BeforeClass
-	public static void setupClass() throws TemplateException {
-		try {
-			DBUpgrader.upgrade();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		TemplateManagerUtil.init();
-	}
-
 	@After
 	public void tearDown() throws Exception {
 		Iterator<ExpandoTable> iterator = _expandoTables.iterator();
