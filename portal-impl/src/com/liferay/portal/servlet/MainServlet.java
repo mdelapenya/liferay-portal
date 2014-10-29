3406e5b772d99ac8834a04105d1098a2dfe171d1
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-10-29 16:24:15 +0100

    LPS-51149 Move the initialization to the listener, so we can make a proper usage of the ServletContext. Weblogic does not allow you to invoke most of the ServletContext methods at the initialisation phase

--- portal-impl/src/com/liferay/portal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/MainServlet.java
@@ -15,100 +15,57 @@
 package com.liferay.portal.servlet;
 
 import com.liferay.portal.NoSuchLayoutException;
-import com.liferay.portal.dao.shard.ShardDataSourceTargetSource;
 import com.liferay.portal.events.EventsProcessorUtil;
-import com.liferay.portal.events.StartupAction;
-import com.liferay.portal.events.StartupHelperUtil;
 import com.liferay.portal.kernel.cache.Lifecycle;
 import com.liferay.portal.kernel.cache.ThreadLocalCacheManager;
-import com.liferay.portal.kernel.deploy.hot.HotDeployUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.plugin.PluginPackage;
 import com.liferay.portal.kernel.servlet.DynamicServletRequest;
 import com.liferay.portal.kernel.servlet.PortalSessionThreadLocal;
 import com.liferay.portal.kernel.servlet.ProtectedServletRequest;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
-import com.liferay.portal.kernel.util.InfrastructureUtil;
 import com.liferay.portal.kernel.util.InstanceFactory;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalLifecycleUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.xml.Document;
-import com.liferay.portal.kernel.xml.DocumentException;
-import com.liferay.portal.kernel.xml.Element;
-import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
-import com.liferay.portal.model.LayoutTemplate;
-import com.liferay.portal.model.Portlet;
-import com.liferay.portal.model.PortletApp;
-import com.liferay.portal.model.PortletFilter;
-import com.liferay.portal.model.PortletURLListener;
-import com.liferay.portal.model.Theme;
 import com.liferay.portal.model.User;
-import com.liferay.portal.plugin.PluginPackageUtil;
-import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.security.jaas.JAASHelper;
-import com.liferay.portal.security.permission.ResourceActionsUtil;
-import com.liferay.portal.server.capabilities.ServerCapabilitiesUtil;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
-import com.liferay.portal.service.LayoutTemplateLocalServiceUtil;
-import com.liferay.portal.service.PortletLocalServiceUtil;
-import com.liferay.portal.service.ResourceActionLocalServiceUtil;
-import com.liferay.portal.service.ThemeLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.servlet.filters.absoluteredirects.AbsoluteRedirectsResponse;
-import com.liferay.portal.servlet.filters.i18n.I18nFilter;
-import com.liferay.portal.setup.SetupWizardUtil;
 import com.liferay.portal.struts.PortletRequestProcessor;
 import com.liferay.portal.struts.StrutsUtil;
 import com.liferay.portal.util.ClassLoaderUtil;
-import com.liferay.portal.util.ExtRegistry;
 import com.liferay.portal.util.MaintenanceUtil;
-import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.ShutdownUtil;
 import com.liferay.portal.util.WebKeys;
-import com.liferay.portlet.PortletBagFactory;
-import com.liferay.portlet.PortletConfigFactoryUtil;
-import com.liferay.portlet.PortletFilterFactory;
-import com.liferay.portlet.PortletInstanceFactoryUtil;
-import com.liferay.portlet.PortletURLListenerFactory;
-import com.liferay.portlet.social.util.SocialConfigurationUtil;
 import com.liferay.util.ContentUtil;
 import com.liferay.util.servlet.EncryptedServletRequest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-import java.util.Set;
-
-import javax.portlet.PortletConfig;
-import javax.portlet.PortletContext;
-import javax.portlet.PortletException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -135,47 +92,6 @@ public class MainServlet extends ActionServlet {
 	@Override
 	public void destroy() {
 		if (_log.isDebugEnabled()) {
-			_log.debug("Destroy plugins");
-		}
-
-		PortalLifecycleUtil.flushDestroys();
-
-		List<Portlet> portlets = PortletLocalServiceUtil.getPortlets();
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Destroy portlets");
-		}
-
-		try {
-			destroyPortlets(portlets);
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Destroy companies");
-		}
-
-		try {
-			destroyCompanies();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Process global shutdown events");
-		}
-
-		try {
-			processGlobalShutdownEvents();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
 			_log.debug("Destroy");
 		}
 
@@ -193,162 +109,6 @@ public class MainServlet extends ActionServlet {
 		servletContext.setAttribute(MainServlet.class.getName(), Boolean.TRUE);
 
 		callParentInit();
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Process startup events");
-		}
-
-		try {
-			processStartupEvents();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-
-			System.out.println(
-				"Stopping the server due to unexpected startup errors");
-
-			System.exit(0);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize server detector");
-		}
-
-		try {
-			initServerDetector();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize plugin package");
-		}
-
-		PluginPackage pluginPackage = null;
-
-		try {
-			pluginPackage = initPluginPackage();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize portlets");
-		}
-
-		List<Portlet> portlets = new ArrayList<Portlet>();
-
-		try {
-			portlets.addAll(initPortlets(pluginPackage));
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize layout templates");
-		}
-
-		try {
-			initLayoutTemplates(pluginPackage, portlets);
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize social");
-		}
-
-		try {
-			initSocial(pluginPackage);
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize themes");
-		}
-
-		try {
-			initThemes(pluginPackage, portlets);
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize web settings");
-		}
-
-		try {
-			initWebSettings();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize extension environment");
-		}
-
-		try {
-			initExt();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Process global startup events");
-		}
-
-		try {
-			processGlobalStartupEvents();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize resource actions");
-		}
-
-		try {
-			initResourceActions(portlets);
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize companies");
-		}
-
-		try {
-			initCompanies();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("Initialize plugins");
-		}
-
-		try {
-			initPlugins();
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		servletContext.setAttribute(WebKeys.STARTUP_FINISHED, true);
-
-		StartupHelperUtil.setStartupFinished(true);
 
 		ThreadLocalCacheManager.clearAll(Lifecycle.REQUEST);
 	}
@@ -588,60 +348,6 @@ public class MainServlet extends ActionServlet {
 			servletContext.getAttribute(TilesUtilImpl.DEFINITIONS_FACTORY));
 	}
 
-	protected void checkWebSettings(String xml) throws DocumentException {
-		Document doc = SAXReaderUtil.read(xml);
-
-		Element root = doc.getRootElement();
-
-		int timeout = PropsValues.SESSION_TIMEOUT;
-
-		Element sessionConfig = root.element("session-config");
-
-		if (sessionConfig != null) {
-			String sessionTimeout = sessionConfig.elementText(
-				"session-timeout");
-
-			timeout = GetterUtil.getInteger(sessionTimeout, timeout);
-		}
-
-		PropsUtil.set(PropsKeys.SESSION_TIMEOUT, String.valueOf(timeout));
-
-		PropsValues.SESSION_TIMEOUT = timeout;
-
-		I18nServlet.setLanguageIds(root);
-		I18nFilter.setLanguageIds(I18nServlet.getLanguageIds());
-	}
-
-	protected void destroyCompanies() throws Exception {
-		long[] companyIds = PortalInstances.getCompanyIds();
-
-		for (long companyId : companyIds) {
-			destroyCompany(companyId);
-		}
-	}
-
-	protected void destroyCompany(long companyId) {
-		if (_log.isDebugEnabled()) {
-			_log.debug("Process shutdown events");
-		}
-
-		try {
-			EventsProcessorUtil.process(
-				PropsKeys.APPLICATION_SHUTDOWN_EVENTS,
-				PropsValues.APPLICATION_SHUTDOWN_EVENTS,
-				new String[] {String.valueOf(companyId)});
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-	}
-
-	protected void destroyPortlets(List<Portlet> portlets) throws Exception {
-		for (Portlet portlet : portlets) {
-			PortletInstanceFactoryUtil.destroy(portlet);
-		}
-	}
-
 	protected HttpServletRequest encryptRequest(
 		HttpServletRequest request, long companyId) {
 
@@ -750,224 +456,6 @@ public class MainServlet extends ActionServlet {
 		}
 	}
 
-	protected void initCompanies() throws Exception {
-		ServletContext servletContext = getServletContext();
-
-		try {
-			String[] webIds = PortalInstances.getWebIds();
-
-			for (String webId : webIds) {
-				PortalInstances.initCompany(servletContext, webId);
-			}
-		}
-		finally {
-			CompanyThreadLocal.setCompanyId(
-				PortalInstances.getDefaultCompanyId());
-
-			ShardDataSourceTargetSource shardDataSourceTargetSource =
-				(ShardDataSourceTargetSource)
-					InfrastructureUtil.getShardDataSourceTargetSource();
-
-			if (shardDataSourceTargetSource != null) {
-				shardDataSourceTargetSource.resetDataSource();
-			}
-		}
-	}
-
-	protected void initExt() throws Exception {
-		ServletContext servletContext = getServletContext();
-
-		ExtRegistry.registerPortal(servletContext);
-	}
-
-	protected void initLayoutTemplates(
-			PluginPackage pluginPackage, List<Portlet> portlets)
-		throws Exception {
-
-		ServletContext servletContext = getServletContext();
-
-		String[] xmls = new String[] {
-			HttpUtil.URLtoString(
-				servletContext.getResource(
-					"/WEB-INF/liferay-layout-templates.xml")),
-			HttpUtil.URLtoString(
-				servletContext.getResource(
-					"/WEB-INF/liferay-layout-templates-ext.xml"))
-		};
-
-		List<LayoutTemplate> layoutTemplates =
-			LayoutTemplateLocalServiceUtil.init(
-				servletContext, xmls, pluginPackage);
-
-		servletContext.setAttribute(
-			WebKeys.PLUGIN_LAYOUT_TEMPLATES, layoutTemplates);
-	}
-
-	protected PluginPackage initPluginPackage() throws Exception {
-		ServletContext servletContext = getServletContext();
-
-		return PluginPackageUtil.readPluginPackageServletContext(
-			servletContext);
-	}
-
-	/**
-	 * @see SetupWizardUtil#_initPlugins
-	 */
-	protected void initPlugins() throws Exception {
-
-		// See LEP-2885. Don't flush hot deploy events until after the portal
-		// has initialized.
-
-		if (SetupWizardUtil.isSetupFinished()) {
-			HotDeployUtil.setCapturePrematureEvents(false);
-
-			PortalLifecycleUtil.flushInits();
-		}
-	}
-
-	protected void initPortletApp(
-			Portlet portlet, ServletContext servletContext)
-		throws PortletException {
-
-		PortletApp portletApp = portlet.getPortletApp();
-
-		PortletConfig portletConfig = PortletConfigFactoryUtil.create(
-			portlet, servletContext);
-
-		PortletContext portletContext = portletConfig.getPortletContext();
-
-		Set<PortletFilter> portletFilters = portletApp.getPortletFilters();
-
-		for (PortletFilter portletFilter : portletFilters) {
-			PortletFilterFactory.create(portletFilter, portletContext);
-		}
-
-		Set<PortletURLListener> portletURLListeners =
-			portletApp.getPortletURLListeners();
-
-		for (PortletURLListener portletURLListener : portletURLListeners) {
-			PortletURLListenerFactory.create(portletURLListener);
-		}
-	}
-
-	protected List<Portlet> initPortlets(PluginPackage pluginPackage)
-		throws Exception {
-
-		ServletContext servletContext = getServletContext();
-
-		String[] xmls = new String[] {
-			HttpUtil.URLtoString(
-				servletContext.getResource(
-					"/WEB-INF/" + Portal.PORTLET_XML_FILE_NAME_CUSTOM)),
-			HttpUtil.URLtoString(
-				servletContext.getResource("/WEB-INF/portlet-ext.xml")),
-			HttpUtil.URLtoString(
-				servletContext.getResource("/WEB-INF/liferay-portlet.xml")),
-			HttpUtil.URLtoString(
-				servletContext.getResource("/WEB-INF/liferay-portlet-ext.xml")),
-			HttpUtil.URLtoString(
-				servletContext.getResource("/WEB-INF/web.xml"))
-		};
-
-		PortletLocalServiceUtil.initEAR(servletContext, xmls, pluginPackage);
-
-		PortletBagFactory portletBagFactory = new PortletBagFactory();
-
-		portletBagFactory.setClassLoader(
-			ClassLoaderUtil.getPortalClassLoader());
-		portletBagFactory.setServletContext(servletContext);
-		portletBagFactory.setWARFile(false);
-
-		List<Portlet> portlets = PortletLocalServiceUtil.getPortlets();
-
-		for (int i = 0; i < portlets.size(); i++) {
-			Portlet portlet = portlets.get(i);
-
-			portletBagFactory.create(portlet);
-
-			if (i == 0) {
-				initPortletApp(portlet, servletContext);
-			}
-		}
-
-		servletContext.setAttribute(WebKeys.PLUGIN_PORTLETS, portlets);
-
-		return portlets;
-	}
-
-	protected void initResourceActions(List<Portlet> portlets)
-		throws Exception {
-
-		for (Portlet portlet : portlets) {
-			List<String> portletActions =
-				ResourceActionsUtil.getPortletResourceActions(portlet);
-
-			ResourceActionLocalServiceUtil.checkResourceActions(
-				portlet.getPortletId(), portletActions);
-
-			List<String> modelNames =
-				ResourceActionsUtil.getPortletModelResources(
-					portlet.getPortletId());
-
-			for (String modelName : modelNames) {
-				List<String> modelActions =
-					ResourceActionsUtil.getModelResourceActions(modelName);
-
-				ResourceActionLocalServiceUtil.checkResourceActions(
-					modelName, modelActions);
-			}
-		}
-	}
-
-	protected void initServerDetector() throws Exception {
-		ServerCapabilitiesUtil.determineServerCapabilities(getServletContext());
-	}
-
-	protected void initSocial(PluginPackage pluginPackage) throws Exception {
-		ClassLoader classLoader = ClassLoaderUtil.getPortalClassLoader();
-
-		ServletContext servletContext = getServletContext();
-
-		String[] xmls = new String[] {
-			HttpUtil.URLtoString(
-				servletContext.getResource("/WEB-INF/liferay-social.xml")),
-			HttpUtil.URLtoString(
-				servletContext.getResource("/WEB-INF/liferay-social-ext.xml"))
-		};
-
-		SocialConfigurationUtil.read(classLoader, xmls);
-	}
-
-	protected void initThemes(
-			PluginPackage pluginPackage, List<Portlet> portlets)
-		throws Exception {
-
-		ServletContext servletContext = getServletContext();
-
-		String[] xmls = new String[] {
-			HttpUtil.URLtoString(
-				servletContext.getResource(
-					"/WEB-INF/liferay-look-and-feel.xml")),
-			HttpUtil.URLtoString(
-				servletContext.getResource(
-					"/WEB-INF/liferay-look-and-feel-ext.xml"))
-		};
-
-		List<Theme> themes = ThemeLocalServiceUtil.init(
-			servletContext, null, true, xmls, pluginPackage);
-
-		servletContext.setAttribute(WebKeys.PLUGIN_THEMES, themes);
-	}
-
-	protected void initWebSettings() throws Exception {
-		ServletContext servletContext = getServletContext();
-
-		String xml = HttpUtil.URLtoString(
-			servletContext.getResource("/WEB-INF/web.xml"));
-
-		checkWebSettings(xml);
-	}
-
 	protected long loginUser(
 			HttpServletRequest request, HttpServletResponse response,
 			long companyId, long userId, String remoteUser)
@@ -1022,19 +510,6 @@ public class MainServlet extends ActionServlet {
 			"this-instance-is-inactive-please-contact-the-administrator");
 
 		return true;
-	}
-
-	protected void processGlobalShutdownEvents() throws Exception {
-		EventsProcessorUtil.process(
-			PropsKeys.GLOBAL_SHUTDOWN_EVENTS,
-			PropsValues.GLOBAL_SHUTDOWN_EVENTS);
-
-		super.destroy();
-	}
-
-	protected void processGlobalStartupEvents() throws Exception {
-		EventsProcessorUtil.process(
-			PropsKeys.GLOBAL_STARTUP_EVENTS, PropsValues.GLOBAL_STARTUP_EVENTS);
 	}
 
 	protected boolean processGroupInactiveRequest(
@@ -1232,12 +707,6 @@ public class MainServlet extends ActionServlet {
 		return true;
 	}
 
-	protected void processStartupEvents() throws Exception {
-		StartupAction startupAction = new StartupAction();
-
-		startupAction.run(null);
-	}
-
 	protected HttpServletRequest protectRequest(
 		HttpServletRequest request, String remoteUser) {
 
@@ -1318,6 +787,6 @@ public class MainServlet extends ActionServlet {
 	private static final String _LIFERAY_PORTAL_REQUEST_HEADER =
 		"Liferay-Portal";
 
-	private static Log _log = LogFactoryUtil.getLog(MainServlet.class);
+	private static final Log _log = LogFactoryUtil.getLog(MainServlet.class);
 
 }
