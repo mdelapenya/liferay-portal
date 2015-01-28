95daf254ecd28a5ce312f53c0a9378a4ff8a0b94
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-01-28 11:23:43 +0100

    LPS-50675 Share the same logic betwen LiferayIntegrationTestRule and the Arquillian Extension

--- modules/test/arquillian-extension/src/com/liferay/arquillian/extension/LiferayTestScenarioExtension.java
+++ modules/test/arquillian-extension/src/com/liferay/arquillian/extension/LiferayTestScenarioExtension.java
@@ -14,8 +14,6 @@
 
 package com.liferay.arquillian.extension;
 
-import com.liferay.arquillian.extension.internal.descriptor.SpringDescriptor;
-import com.liferay.arquillian.extension.internal.descriptor.SpringDescriptorImpl;
 import com.liferay.arquillian.extension.internal.observer.InitializeLiferayTestEnvironment;
 
 import org.jboss.arquillian.core.spi.LoadableExtension;
@@ -28,9 +26,6 @@ public class LiferayTestScenarioExtension implements LoadableExtension {
 	@Override
 	public void register(ExtensionBuilder extensionBuilder) {
 		extensionBuilder.observer(InitializeLiferayTestEnvironment.class);
-
-		extensionBuilder.service(
-			SpringDescriptor.class, SpringDescriptorImpl.class);
 	}
 
 }
