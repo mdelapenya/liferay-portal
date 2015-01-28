189c9801aa55cf14f9a26e5e9efe8c15226de235
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-01-28 12:41:36 +0100

    LPS-50675 Change the package of the persistence test generated (In osgi two bundles can't share the same package)

--- portal-impl/src/com/liferay/portal/tools/servicebuilder/ServiceBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/servicebuilder/ServiceBuilder.java
@@ -2584,6 +2584,16 @@ public class ServiceBuilder {
 	private void _createPersistenceTest(Entity entity) throws Exception {
 		Map<String, Object> context = _getContext();
 
+		File ejbFile = new File(
+			_testOutputPath + "/service/persistence/" + entity.getName() +
+				"PersistenceTest.java");
+
+		if (ejbFile.exists()) {
+			System.out.println("Removing test file " + ejbFile);
+
+			ejbFile.delete();
+		}
+
 		context.put("entity", entity);
 
 		// Content
@@ -2592,8 +2602,8 @@ public class ServiceBuilder {
 
 		// Write file
 
-		File ejbFile = new File(
-			_testOutputPath + "/service/persistence/" + entity.getName() +
+		ejbFile = new File(
+			_testOutputPath + "/service/persistence/test/" + entity.getName() +
 				"PersistenceTest.java");
 
 		writeFile(ejbFile, content, _author);
