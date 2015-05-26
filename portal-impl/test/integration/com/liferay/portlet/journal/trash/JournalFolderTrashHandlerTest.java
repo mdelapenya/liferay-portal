8b28fad0688db91238c70ef2d8375da4ce932516
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-05-26 16:47:23 +0200

    LPS-55878 Exclude tests

--- portal-impl/test/integration/com/liferay/portlet/journal/trash/JournalFolderTrashHandlerTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/trash/JournalFolderTrashHandlerTest.java
@@ -55,6 +55,12 @@ public class JournalFolderTrashHandlerTest extends BaseTrashHandlerTestCase {
 	@Ignore()
 	@Override
 	@Test
+	public void testAddBaseModelTrashInitialConditionsDraft() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
 	public void testTrashAndDeleteDraft() throws Exception {
 	}
 
