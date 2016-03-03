f3d268a6f96ea59507d7c955451aba8bf3e83e02
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2016-03-03 09:51:01 +0100

    LPS-58170 Move the modified logic to its own method, since we need to iterate over all the tracked bundles at any time the configuration is changed

--- modules/apps/platform/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/LiferayTemplateClassResolver.java
+++ modules/apps/platform/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/LiferayTemplateClassResolver.java
@@ -128,7 +128,6 @@ public class LiferayTemplateClassResolver implements TemplateClassResolver {
 	}
 
 	@Activate
-	@Modified
 	protected void activate(
 		BundleContext bundleContext, Map<String, Object> properties) {
 
@@ -141,17 +140,31 @@ public class LiferayTemplateClassResolver implements TemplateClassResolver {
 
 		_classResolverBundleTracker.open();
 
-		Set<ClassLoader> allowedClassLoaders = _findAllowedClassLoaders(
-			_freemarkerEngineConfiguration.allowedClasses(), bundleContext);
-
-		_whiteListedClassloaders.addAll(allowedClassLoaders);
-
-		_whiteListedClassloaders.add(getClass().getClassLoader());
+		_whiteListedClassloaders.add(
+			LiferayTemplateClassResolver.class.getClassLoader());
 	}
 
 	@Deactivate
 	protected void deactivate() {
 		_classResolverBundleTracker.close();
+	}
+
+	@Modified
+	protected void modified(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		_freemarkerEngineConfiguration = Configurable.createConfigurable(
+			FreeMarkerEngineConfiguration.class, properties);
+
+		for (Bundle bundle : _bundles) {
+			ClassLoader classLoader = _findClassLoader(
+				_freemarkerEngineConfiguration.allowedClasses(),
+				bundle.getBundleContext());
+
+			if (classLoader != null) {
+				_whiteListedClassloaders.add(classLoader);
+			}
+		}
 	}
 
 	private ClassLoader _findClassLoader(
@@ -273,6 +286,7 @@ public class LiferayTemplateClassResolver implements TemplateClassResolver {
 	private static final Log _log = LogFactoryUtil.getLog(
 		LiferayTemplateClassResolver.class);
 
+	private final Set<Bundle> _bundles = new ConcurrentHashSet<>();
 	private BundleTracker<ClassLoader> _classResolverBundleTracker;
 	private volatile FreeMarkerEngineConfiguration
 		_freemarkerEngineConfiguration;
@@ -294,6 +308,8 @@ public class LiferayTemplateClassResolver implements TemplateClassResolver {
 				_whiteListedClassloaders.add(classLoader);
 			}
 
+			_bundles.add(bundle);
+
 			BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
 
 			return bundleWiring.getClassLoader();
@@ -309,6 +325,8 @@ public class LiferayTemplateClassResolver implements TemplateClassResolver {
 			Bundle bundle, BundleEvent bundleEvent, ClassLoader classLoader) {
 
 			_whiteListedClassloaders.remove(classLoader);
+
+			_bundles.remove(bundle);
 		}
 
 	}
